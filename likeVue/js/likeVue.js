40468af51b7f08f1ff1e363a73895f6c0d9beb29
何珂 <yunkehe@163.com>
2016-09-14 17:05:25 +0800

    likeVue

--- likeVue/js/likeVue.js
+++ likeVue/js/likeVue.js
@@ -1,14 +1,3 @@
-// var obj = {"name": "heke", "age": 26};
-
-// var _obj = _.extend(obj, Backbone.Events);
-
-// function handleChange(e){
-// 	console.log("e", e);
-
-// };
-
-// _obj.on("change", handleChange);
-
 
 (function(root){
 	var prev = root.likeVue;
