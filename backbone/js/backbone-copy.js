b5bd6b04d6963269d22eaad35c59fe8d5933a1f2
何珂 <yunkehe@163.com>
2016-11-10 18:00:31 +0800

    backbone.source.js

--- backbone/js/backbone-copy.js
+++ backbone/js/backbone-copy.js
@@ -28,11 +28,9 @@
 		return this;
 	}
 
+	// 是否需要模拟http/json 用于不支持REST/ HTTP和json的情况下使用 
 	Backbone.emulateHTTP = false;
-
 	Backbone.emulateJSON = false;
-
-
 
 	var Events = Backbone.Events = {
 
@@ -428,6 +426,66 @@
 			return this.sync('read', this, options);
 		},
 		
+		// 保存模型到服务器
+		save: function(key, val, options){
+			var attrs, xhr, method, attributes = this.attributes;
+
+			if(key == null || typeof key === 'object'){
+				attrs = key;
+				options = val;
+			}else{
+				(attrs = {})[key] = val;
+			}
+
+			// attrs 组合后的键值对
+
+			// 设置验证
+			options = _.extend({validate: true}, options);
+
+			// 不需要等待服务器保存成功
+			if(attrs && !options.wait){
+				// 设置失败
+				if(!this.set(attrs, options)) return false;
+			}else{
+				// 验证失败
+				if(!this._validate(attrs, options)) return false;
+			}
+
+			// 等待服务器保存成功
+			if(attrs && options.wait){
+				this.attributes = _.extend({}, attributes, attrs);
+			}
+
+			if(options.parse === void 0) options.parse = true;
+
+			var model = this;
+			var success = options.success;
+			options.success = function(resp){
+				model.attributes = attributes;
+				var serverAttrs = model.parse(resp, options);
+				if(options.wait) serverAttrs = _.extend(attrs || {}, serverAttrs);
+
+				// what did this work?
+				// response不是对象 并且 serverAttrs失败
+				if(_.isObject(serverAttrs) && !this.set(serverAttrs, options)){
+					return false;
+				}
+
+				if(success) success(model, resp, options);
+				model.trigger('sync', model, resp, options);
+			}
+
+			wrapError(this, options);
+
+			method = this.isNew() ? 'create' : (options.patch ? 'patch' : 'update');
+			if(method === 'patch' && !options.attrs) options.attrs = attrs;
+			xhr = this.sync(method, this, options);
+
+			if(attrs && options.wait) this.attributes = attributes;
+
+			return xhr; 
+		},
+
 		// 仅简单返回后台实际返回的response
 		// 可以自己重载进行修改
 		parse: function(resp, options){
@@ -438,6 +496,7 @@
 		sync: function(){
 			return Backbone.sync.apply(this, arguments);
 		},
+
 		// 内部使用 判断验证
 		_validate: function(attrs, options){
 			// 没有设置验证时 总是通过验证
@@ -460,7 +519,64 @@
  */
 
 	Backbone.sync = function(method, model, options){
+		var type = methodMap[method];
 
+		// 默认参数
+		_.defaults(options || (options = {}),{
+			emulateHTTP: Backbone.emulateHTTP,
+			emulateJSON: Backbone.emulateJSON
+		})
+
+		var params = {type: type, 'dataType': 'json'};
+
+		// 确保有一个url
+		if(!options.url){
+			params.url = _.result(model, 'url') || urlError();
+		};
+
+		if(options.data == null && model && (method === 'create' || method === 'update' || method === 'patch')){
+			params.contentType = 'application/json';
+			params.data = JSON.stringify(options.attrs || model.toJSON(options));
+		};
+
+		// 仿真json
+		if(options.emulateJSON){
+			params.contentType = 'application/x-www-form-urlencoded';
+			params.data = params.data ? {model: params.data} : {};
+		}
+
+		if(options.emulateHTTP && (type === 'PUT' || type === 'DELETE' || type === 'PATCH')){
+			params.type = 'POST';
+			if(options.emulateJSON) params.data._method = type;
+			var beforeSend = options.beforeSend;
+			options.beforeSend = function(xhr){
+				xhr.setRequestHeader('X-HTTP-Method-Override', type);
+				if(beforeSend) return beforeSend.apply(this, arguments);
+			}
+		}
+
+		if(params.type !== 'GET' && !options.emulateJSON){
+			params.processData = false;
+		}
+
+		var xhr = options.xhr = Backbone.ajax(_.extend(params, options));
+		model.trigger('request', model, xhr, options);
+
+		return xhr;
+
+	};
+
+	// CRUD 对应 http请求方法
+	var methodMap = {
+		'create': 'POST',
+		'read': 'GET',
+		'update': 'PUT',
+		'delete': 'DELETE',
+		'patch': 'PATCH'
+	};
+
+	Backbone.ajax = function(){
+		return Backbone.$.ajax.apply(Backbone.$, arguments);
 	};
 
 	// 继承方法
@@ -497,7 +613,11 @@
 
 	// 设置继承
 	Model.extend = extend;
-	 
+	
+	var urlError = function(){
+		throw new Error('A "url" property must be specified!');
+	};
+
 	var wrapError = function(model, options){
 		var error = options.error;
 		options.error = function(resp){
